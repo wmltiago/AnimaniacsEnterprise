3e0fc1456d3a20a72ff3b74266c8349b7a5ac9a9
wmltiago <tiagobhs@outlook.com>
2016-08-21 17:44:11 -0300

    Tiago

--- php/cadastro.php
+++ php/cadastro.php
@@ -1,15 +1,13 @@
 <?php
-try{
-$conexao=new PDO("mysql:host=localhost;dbname=acessoria_esportiva","root","password");
-}catch(PDOException $e){ "Codigo do Erro: ".$e->getCode()." Erro: ".$e->getMessage();
-}
+
+include("conexao.php");
 
 
 try{
 #COMEÇO DO IF
-$stmt = $conexao->prepare("insert into usuario (nome, telefone, cpf,rg, endereco, data_nascimento, sexo, email, senha) values(?,?,?,?,?,?,?,?,?)");
+$stmt = $conexao->prepare("insert into usuario (nome, telefone, cpf,rg, endereco, data_nascimento, sexo, email, senha, login) values(?,?,?,?,?,?,?,?,?,?)");
 
-$nome=$_POST["nome"]; $telefone=$_POST["telefone"]; $cpf=$_POST["cpf"]; $rg=$_POST["rg"]; $endereco=$_POST["endereco"]; $data_nascimento=$_POST["data_nascimento"];$sexo=$_POST["sexo"];$email=$_POST["email"];$senha=$_POST["senha"];
+$nome=$_POST["nome"]; $telefone=$_POST["telefone"]; $cpf=$_POST["cpf"]; $rg=$_POST["rg"]; $endereco=$_POST["endereco"]; $data_nascimento=$_POST["data_nascimento"];$sexo=$_POST["sexo"];$email=$_POST["email"];$senha=$_POST["senha"];$login=$_POST["login"];
 
 $stmt->bindParam(2,$telefone);
 $stmt->bindParam(1,$nome);
@@ -20,6 +18,7 @@ $stmt->bindParam(6,$data_nascimento);
 $stmt->bindParam(7,$sexo);
 $stmt->bindParam(8,$email);
 $stmt->bindParam(9,$senha);
+$stmt->bindParam(10,$login);
 
 #$stmt->bindValue(1,1);
 #$stmt->bindValue(2,"Zé Maria");
