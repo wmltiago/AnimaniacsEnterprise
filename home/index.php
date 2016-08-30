28c3897b3e4c0ad90f8042823b0800d4455f008d
wmltiago <tiagobhs@outlook.com>
2016-08-30 17:12:15 -0300

    Tiago

--- home/index.php
+++ home/index.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "@luno1fpe";
+$pass = "password";
 $banco = "acessoria_esportiva";
 $conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
 mysql_select_db($banco) or die (mysql_error());
@@ -25,158 +25,14 @@ mysql_select_db($banco) or die (mysql_error());
 	
 	</head>
 <body>
-<?php
-session_start();
-	if (isset($_SESSION["login"])){ 
-$login = $_SESSION['login'];
- echo "<center>seja bem vindo $login</center>";
 
-?>
-<div id="link">
-<a href='sair.php'>Sair</a>
-</div>
+
 <?php
 
-}else{
+include "../php/cabecalho_include.php"; //Inclusao do cabeçalho 
+
 
 ?>
-
-
-<div id="tarja_pre_cabecalho" >
-<a href="../home/login.html" class="sem_linha" >Login </a>
-<a href="../cadastro/cadastro_cliente.html" target="blank" class="sem_linha2" >Cadastre-se</a>
-</div>
-<?php
-}
-?>
-	
-<div id="cabecalho">
-
-<img src="../imagens/a.png">
-
-
-		<h1>AE Assessoria Esportiva </h1>
-		<br>
-		<h2> O melhor conceito em saúde e bem estar! </h2>
-	
-		<!-- em Aberto <input type="button" value="    cadastre-se    " id="but" onclick="Windows();">  BOTÃO CADASTRO -->
-	
-</div>
-
-<div id='cssmenu'>  <!-- MENU INDEX EM ABERTO-->
-  <ul>
-     <li class='active'><a href="..\home/index.html">Home</a></li>
-     <li class='has-sub '><a href="..\home/empresa.html" target="blank">Empresa</a>
-        <!--<ul>
-           <li class='has-sub '><a href='..\[secretaria]/cadastrar_professor.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-           <li class='has-sub '><a href='..\[secretaria]/cadastrar_cursos_disciplinas.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   <li class='has-sub '><a href='..\[secretaria]/realizar_matricula.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   <li class='has-sub '><a href='..\[secretaria]/emitir_historico.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-        </ul>-->
-     </li>
-     <li><a href="">Serviços</a>
-	 <ul>
-           <li class='has-sub '><a href='..\servicos/crossfit.html'>CrossFit</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>-->
-           </li>
-           <li class='has-sub '><a href='..\servicos/funcional.html'>Funcional</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>-->
-           </li>
-		   <li class='has-sub '><a href='..\servicos/musculacao.html'>Musculação</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>-->
-           </li>
-		   
-        </ul>
-		</li>
-     <li><a href="..\dicas/dicas.html">Dicas</a>
-	 <ul>
-           <!--<li class='has-sub '><a href='..\dicas/dicas.html'>Dicas</a> em aberto
-             <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>-->
-     		   
-     </ul>
-		</li>
-		<li><a href="..\equipe/membros.html">Equipe</a>
-	<!-- <ul>
-           <li class='has-sub '><a href='..\[professor]/registrar_faltas.html'>xxxxxxxxxx</a>
-              <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-           <li class='has-sub '><a href='..\[professor]/registrar_notas.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   <li class='has-sub '><a href='..\[professor]/registrar_projeto_pesquisa.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   
-        </ul>-->
-		</li>
-		<li><a href="..\galeria/fotos.html">Galeria</a>
-	 <!--<ul>
-           <li class='has-sub '><a href='..\[professor]/registrar_faltas.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-           <li class='has-sub '><a href='..\[professor]/registrar_notas.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   <li class='has-sub '><a href='..\[professor]/registrar_projeto_pesquisa.html'>xxxxxxxxxx</a>
-             <!-- <ul>
-                 <li><a href='#'>Sub Item</a></li>
-                 <li><a href='#'>Sub Item</a></li>
-              </ul>
-           </li>
-		   
-        </ul>-->
-		</li>
-	  <li><a href="..\suporte/suporte.html" target="_blank">Suporte</a></li>
-  </ul>
-</div>
 	<div id="x">
 <div id="carrossel"><!-- INICIO DO CARROSEL -->
 <center>
--- home/index2.php~
+++ home/index2.php~
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$pass = "@luno1fpe";
+$pass = "password";
 $banco = "acessoria_esportiva";
 $conexao = mysql_connect($host, $user, $pass) or die(mysql_error());
 mysql_select_db($banco) or die (mysql_error());
